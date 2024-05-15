986bf0e738cfe4f37279316f025d338efdcdb185
Liquidator04 <aritrabag04@gmail.com>
2024-05-15 19:23:46 +0530

    test

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 
 // To connect with your mongoDB database
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI, {
 	dbName: 'UserInformation',
 	useNewUrlParser: true,
